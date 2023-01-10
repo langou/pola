10e06b7c4a0dfc49a9f4cca116d3afe90c49feff
Julien Langou <julien.langou@ucdenver.edu>
2023-01-09 21:32:30 -0700

    a little bit ashamed of me, but I do not think I initially got CGS2 correct at my first attempt this is the correct CGS2

--- qr_cgs2_v1.c
+++ qr_cgs2_v1.c
@@ -5,35 +5,32 @@
 void qr_cgs2_v1 (int M, int N, double Q[M][N], double R[N][N], double tmp[N] )
 {
   int i, j, k;
-  double nrm;
 
 #pragma scop
-for (k = 0; k < N; k++) {
-   nrm = 0.0e+00;
-   for (i = 0; i < M; i++)
-      nrm += Q[i][k] * Q[i][k];
-   R[k][k] = sqrt(nrm);
-   for (i = 0; i < M; i++)
-      Q[i][k] /= R[k][k];
-   for (j = k + 1; j < N; j++) {
-      R[k][j] = 0.0e+00;
-      for (i = 0; i < M; i++)
-         R[k][j] += Q[i][k] * Q[i][j];
+for (j = 0; j < N; j++) {
+   for (i = 0; i < j; i++) {
+      R[i][j] = 0.0e+00;
+      for (k = 0; k < M; k++)
+         R[i][j] += Q[k][i] * Q[k][j];
    }
-   for (j = k + 1; j < N; j++) {
-      for (i = 0; i < M; i++)
-         Q[i][j] -= Q[i][k] * R[k][j];
+   for (i = 0; i < j; i++)
+      for (k = 0; k < M; k++)
+         Q[k][j] -= Q[k][i] * R[i][j];
+   for (i = 0; i < j; i++) {
+      tmp[i] = 0.0e+00;
+      for (k = 0; k < M; k++)
+         tmp[i] += Q[k][i] * Q[k][j];
+      R[i][j] += tmp[i];
    }
-   for (j = k + 1; j < N; j++) {
-      tmp[j] = 0.0e+00;
-      for (i = 0; i < M; i++)
-         tmp[j] += Q[i][k] * Q[i][j];
-      R[k][j] += tmp[j];
-   }
-   for (j = k + 1; j < N; j++) {
-      for (i = 0; i < M; i++)
-         Q[i][j] -= Q[i][k] * tmp[j];
-   }
+   for (i = 0; i < j; i++)
+      for (k = 0; k < M; k++)
+         Q[k][j] -= Q[k][i] * tmp[i];
+   R[j][j] = 0.0e+00;
+   for (k = 0; k < M; k++)
+      R[j][j] += Q[k][j] * Q[k][j];
+   R[j][j] = sqrt(R[j][j]);
+   for (k = 0; k < M; k++)
+      Q[k][j] /= R[j][j];
 }
 #pragma endscop
 
