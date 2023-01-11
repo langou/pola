8923ea247b34791369b65872822e868ff15f0b84
Julien Langou <julien.langou@ucdenver.edu>
2023-01-11 09:36:09 -0700

    ping

--- qr_mgs_v1_ll.c
+++ qr_mgs_v1_ll.c
@@ -8,18 +8,18 @@ int i, j, k;
 #pragma scop
 for (j = 0; j < N; j++) {
    for (i = 0; i < j; i++) {
-      R[i][j] = 0.0e+00;
+      R[i][j] = 0.0e+00;                // LL2  RL6
       for (k = 0; k < M; k++)
-         R[i][j] += A[k][i] * A[k][j];
-      for (k = 0; k < M; k++)
-         A[k][j] -= A[k][i] * R[i][j];
-   }
-   R[j][j] = 0.0e+00;
+         R[i][j] += A[k][i] * A[k][j];  // LL3  RL7
+      for (k = 0; k < M; k++)           
+         A[k][j] -= A[k][i] * R[i][j];  // LL4  RL8
+   }                                    
+   R[j][j] = 0.0e+00;                   // LL5  RL2   
+   for (k = 0; k < M; k++)              
+      R[j][j] += A[k][j] * A[k][j];     // LL6  RL3    
+   R[j][j] = sqrt(R[j][j]);             // LL7  RL4
    for (k = 0; k < M; k++)
-      R[j][j] += A[k][j] * A[k][j];
-   R[j][j] = sqrt(R[j][j]);
-   for (k = 0; k < M; k++)
-      A[k][j] /= R[j][j];
+      A[k][j] /= R[j][j];               // LL8  RL5
 }
 #pragma endscop
 }
