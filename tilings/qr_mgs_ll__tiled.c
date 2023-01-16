9252873b1313248b8626051ad3f9bb1ff556631a
Julien Langou <julien.langou@ucdenver.edu>
2023-01-15 22:29:44 -0700

    ping

---
+++ tilings/qr_mgs_ll__tiled.c
@@ -0,0 +1,42 @@
+#include <math.h>
+
+// qr_mgs_ll
+// this is the left-looking variant of MGS
+
+void qr_mgs_ll__tiled (int M, int N, int B, double A[M][N], double R[N][N] )
+{
+int i, j, k;
+int j0;
+#pragma scop
+//for (j = 0; j < N; j++) {
+for (j0 = 0; j0 < N; j0+=B) {
+// read A(1:M,i0:i0+B)
+   for (i = 0; i < j0; i++) {
+//    read A(1:M,j)
+      for (j = j0; ((j < j0+B)&&(j < N)); j++) {
+         R[i][j] = 0.0e+00;
+         for (k = 0; k < M; k++)
+            R[i][j] += A[k][i] * A[k][j];
+         for (k = 0; k < M; k++)
+            A[k][j] -= A[k][i] * R[i][j];
+      }
+   }
+   for (j = j0; ((j < j0+B)&&(j < N)); j++) {
+      for (i = j0; i < j; i++) {
+         R[i][j] = 0.0e+00;
+         for (k = 0; k < M; k++)
+            R[i][j] += A[k][i] * A[k][j];
+         for (k = 0; k < M; k++)
+            A[k][j] -= A[k][i] * R[i][j];
+      }
+      R[j][j] = 0.0e+00;
+      for (k = 0; k < M; k++)
+         R[j][j] += A[k][j] * A[k][j];
+      R[j][j] = sqrt(R[j][j]);
+      for (k = 0; k < M; k++)
+         A[k][j] /= R[j][j];
+   }
+// write A(1:M,i0:i0+B)
+}
+#pragma endscop
+}
