6f53d78e5c142b2582422dde0c491534c60bfa2e
Julien Langou <julien.langou@ucdenver.edu>
2023-03-07 15:34:50 -0700

    ping

--- gs/qr_mgs_rec_blas.c
+++ gs/qr_mgs_rec_blas.c
@@ -23,13 +23,25 @@ if( n == 1 ){
 
    qr_mgs_rec_blas ( m, n1, A, lda, R, ldr );
 
+
+// for (i = 0; i < n1; i++) {
+//    for (int j = n1; j < n; j++) {
+//       R[i+j*ldr] = 0.0e+00;
+//       for (k = 0; k < m; k++)
+//          R[i+j*ldr] += A[k+i*lda] * A[k+j*lda];
+//       for (k = 0; k < m; k++)           
+//          A[k+j*lda] -= A[k+i*lda] * R[i+j*ldr];
+//    }
+// }
+
+// for (i = 0; i < n1; i++) {
+//    for (int j = n1; j < n; j++) {
+//       R[i+j*ldr] = cblas_ddot( m, &(A[i*lda]), 1, &(A[j*lda]), 1 );
+//       cblas_daxpy( m, -R[i+j*ldr], &(A[i*lda]), 1, &(A[j*lda]), 1 );
+//    }
+// }
+
    for (i = 0; i < n1; i++) {
-
-//     for (int j = n1; j < n; j++) {
-//        R[i+j*ldr] = cblas_ddot( m, &(A[i*lda]), 1, &(A[j*lda]), 1 );
-//        cblas_daxpy( m, -R[i+j*ldr], &(A[i*lda]), 1, &(A[j*lda]), 1 );
-//     }
-
       cblas_dgemv( CblasColMajor, CblasTrans, m, n2,
                   1.0, &(A[n1*lda]), lda, &(A[i*lda]), 1,
                   0.0, &(R[i+n1*ldr]), ldr );
