81a5bff8ef17dc5373e218368e0e3bd87f7e50c0
Julien Langou <julien.langou@ucdenver.edu>
2023-07-10 05:27:21 -0600

    fix bug, thanks to Lionel for letting me know

--- hh/script_check_v2q.sh
+++ hh/script_check_v2q.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_rl
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_rl_blas
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_rec_blas
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_ll
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_ll_blas
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_ll_tiled
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_ll_tiled_blas
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_rl_tiled
-./main_v2q__time.exe -m 19 -n 13 -method hh_v2q_rl_tiled_blas
-./main_v2q__time.exe -m 19 -n 13 -method org2r
-./main_v2q__time.exe -m 19 -n 13 -method orgqr
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_rl
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_rl_blas
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_rec_blas
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_ll
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_ll_blas
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_ll_tiled
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_ll_tiled_blas
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_rl_tiled
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method hh_v2q_rl_tiled_blas
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method org2r
+./main_v2q__time.exe -m 19 -n 13 -b 5 -method orgqr
 
 
 
